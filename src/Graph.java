27759ae7fda169baff2840c75c6bc8002ea5b169
RukshanDias <rukshandias002@gmail.com>
2023-04-12 16:39:34 +0530

    created Graph interface

---
+++ src/Graph.java
@@ -0,0 +1,9 @@
+/**
+ * @author Rukshan Dias (w1912792)
+ */
+public interface Graph {
+    void addEdge();
+    void removeEdge();
+    void displayGraph();
+    void importFromFile(String filename);
+}
